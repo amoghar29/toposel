a10905eb54d0b5a80f362c6755a27094a7167962
amogha r <amoghar23@gmail.com>
2025-02-14 19:50:25 +0530

    final commit

--- controllers/userController/search.js
+++ controllers/userController/search.js
@@ -1,16 +1,29 @@
 const User = require("../../models/User");
+
 const searchUsers = async (req, res) => {
   try {
     const { query } = req.query;
+
+    if (!query || typeof query !== "string") {
+      return res.status(400).json({ message: "Search query is required" });
+    }
 
     const users = await User.findOne({
       $or: [
         { username: { $regex: query, $options: "i" } },
         { email: { $regex: query, $options: "i" } },
       ],
-    }).select("-password");
-    res.json(users);
+    })
+      .select("-password") 
+      
+
+    if (!users || users.length === 0) {
+      return res.status(404).json({ message: "No user found" });
+    }
+
+    res.json({ users });
   } catch (error) {
+    console.error("Search error:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
